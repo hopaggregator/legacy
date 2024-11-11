77a581599d4380871298fa4307de8cadb4853acc
bonkman22 <bonkman223@gmail.com>
2024-11-11 13:30:05 -0500

    added bonded out coin metadata

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import type { CoinMetadata } from "@mysten/sui";
 
 import legacyCoinTypesData from "./legacy/data/legacy_coin_types.json" assert { type: "json" };
 import legacyCoinMetadataData from "./legacy/data/legacy_coin_metadata.json" assert { type: "json" };
+import bondedCoinMetadataData from "./legacy/data/bonded_out_metadata.json" assert { type: "json" };
 
 export { getLegacyMetadata } from "./legacy/resolver.js";
 
 // Assign types to the JSON imports
 export const LegacyCoinTypes = legacyCoinTypesData as string[];
 export const LegacyCoinMetadata = legacyCoinMetadataData as Record<string, CoinMetadata>;
+export const BondedOutCoinMetadata = bondedCoinMetadataData as Record<string, CoinMetadata>;
